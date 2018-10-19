4bec9967dc4d0f29360bb32cf190af03b1f132e9
Tom Rowson <tom.rowson@gmail.com>
2018-10-19 13:39:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,14 @@
 def oxford_comma(array)
-  case array.length 
+  case array.length
+    when 0
+      return nil
     when 1
       return array[0]
     when 2
       return array.join(" and ")
   else 
-    return nil
+    array[array.length - 1]
+    final_item
+    return array.join()
   end
 end
